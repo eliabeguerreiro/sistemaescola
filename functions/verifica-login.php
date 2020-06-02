40280372147a40eedf1c6b8018fce0518ce44098
Eliabe <eliabepaz.wprk@gmail.com>
2020-06-02 10:44:44 -0300

    igualando versões

--- functions/verifica-login.php
+++ functions/verifica-login.php
@@ -10,12 +10,17 @@ if($btnLogin){
 		$resultado_usuario = mysqli_query($conn, $result_usuario);
 		if($resultado_usuario){
 			$row_usuario = mysqli_fetch_assoc($resultado_usuario);
+			ob_start();
 			if(password_verify($senha, $row_usuario['senha'])){
 				$_SESSION['id'] = $row_usuario['id'];
 				$_SESSION['nome'] = $row_usuario['nome'];
 				$_SESSION['matricula'] = $row_usuario['matricula'];
 				$_SESSION['tipo'] = $row_usuario['tipo'];
-				header("Location: ../src.php?pg=painel");
+				$nh = '';
+    			$nh .='Dia: '. date('d/m/Y') .' Hora: '.date('h:i:s');
+				$relat = "INSERT INTO `entrada` (`matricula`, `entrada`) VALUES ('" .$_SESSION['matricula']. "','$nh')";
+				$resultado_usario = mysqli_query($conn, $relat);
+				header("Location:../src.php?pg=painel");
 			}else{
 				$_SESSION['msg'] = "Login e senha incorreto!";
 				header("Location: ../index.php");
@@ -29,3 +34,4 @@ if($btnLogin){
 	$_SESSION['msg'] = "Página não encontrada";
 	header("Location: ../index.php");
 }
+ob_end_flush();

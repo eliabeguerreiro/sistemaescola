b23daa8d65b1861abcc60c7148c262e1efb58600
Eliabe <eliabepaz.wprk@gmail.com>
2020-06-25 11:23:36 -0300

    pesquisa de presença por sala

--- relatturma.php
+++ relatturma.php
@@ -1,11 +1,17 @@
 <?php
 include_once('functions/conexão.php');
 
-$result = "SELECT * FROM ususarios WHERE tipo = /*aqui vem a sala */";
-
+$result = "SELECT * FROM ususarios WHERE tipo ='" .$_POST['sala']. "'";
 $resultildo = mysqli_query($conn, $result);
-
-
 $resultado = mysqli_fetch_array($resultildo);
 
-//percorrer o array
+$cont = 0;
+foreach($resultado['matricula'] as $aluno){
+    $res = "SELECT * FROM sessoes WHERE matricula='" .$_aluno['matricula']. "' and dia='" .$_POST['dia']. "' limit-1";
+    $resul = mysqli_query($conn, $res);
+    if (mysqli_num_rows($resul = 1)){
+        $frequencia[$cont] = 'Presente';     
+    }else{
+        $frequencia[$cont] = 'Faltou';
+    }
+}
